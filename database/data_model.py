60ea55541cd94d5de2eb6b3094418d5b220154d5
biswajeet <biswajeet@isa-software.com>
2022-01-24 00:16:27 +0100

    added database functionality

--- database/data_model.py
+++ database/data_model.py
@@ -16,3 +16,33 @@ def insert_into_tables(orders, barcodes):
         barcodes.to_sql('barcodes', db, if_exists='append', index=False)
     except Exception as err:
         raise Exception('Error Occured while insering into tables :', err)    
+
+def fetch_top_5():
+    try:
+        result_set = db.execute("SELECT customer_id, COUNT(*) as amount_of_tickets FROM orders, barcodes where orders.order_id=barcodes.order_id GROUP BY customer_id ORDER BY amount_of_tickets DESC LIMIT 5")
+        print('customer_id \t'+'amount_of_tickets')
+        for r in result_set:  
+            print(str(r['customer_id'])+'\t\t'+str(r['amount_of_tickets']))
+    except Exception as err:
+        raise Exception('Error Occured while fetching top 5 customers from db :', err)    
+        
+
+def fetch_unused_barcodes():
+    try:
+        result_set = db.execute("SELECT barcode FROM barcodes where order_id IS NULL")
+        print('Unused_Barcodes')
+        for r in result_set:  
+            print(r['barcode'])
+    except Exception as err:
+        raise Exception('Error Occured while fetching unused barcodes from db :', err) 
+        
+        
+def fetch_output():
+    try:
+        result_set = db.execute("SELECT customer_id, orders.order_id, STRING_AGG(barcode, ',') FROM orders, barcodes where orders.order_id=barcodes.order_id GROUP BY customer_id, orders.order_id ORDER BY customer_id, orders.order_id")
+        print('customer_id \t'+'order_id \t'+ 'barcodes')
+        for r in result_set:  
+            print(str(r[0])+'\t\t'+str(r[1])+'\t\t'+str(r[2]))
+    except Exception as err:
+        raise Exception('Error Occured while fetching output from db :', err) 
+        
